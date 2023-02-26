f174d241c67c7daaa8a335ce0fbf6a6554d26c4c
ShawonKhondoker <shawonkhondoker0@gmail.com>
2023-02-26 14:25:39 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+
+var firstArray =[1,2,3,4,5,6,7,8,9];
+ console.log(firstArray);
+
+var firstArray2 =["Shawon","Royes","Esha" ];
+console.log(firstArray2.length);
+
+
+var firstArray3 =["Shawon","Royes","Esha" ];
+console.log(firstArray3[2]);
+
+var firstArray4 = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,];
+
+console.log(firstArray4.indexOf(30));
+
+var firstArray4 = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,];
+
+console.log(firstArray4.indexOf(35));
+
+console.log(firstArray4.indexOf(40));
+
+//apudded
+var firstArray6 =["Alu","Potol","Tomatu","Gajor" ];
+firstArray6[0]="vandi";
+firstArray6[2]="corola";
+firstArray6.push(10);
+firstArray6.push("amar")
+
+
+console.log(firstArray6);
+
+//apudded
+
+var firstArray7 =["Alu","Potol","Tomatu","Gajor" ];
+ firstArray7.pop();
+console.log(firstArray7);
+
+var firstArray8 =["Alu","Potol","Tomatu","Gajor" ];
+ firstArray8.shift();
+ firstArray8.shift();
+ 
+
+ 
+ console.log(firstArray8)
+
+
+var firstArray9 =["Alu","Potol","Tomatu","Gajor" ];
+//  firstArray9.unshift("lemon");
+//  firstArray9.unshift("love");
+ firstArray9.slice();
+
+ console.log(firstArray9)
+
+
+
+
+var var1 =23;
+var1=var1+20;
+var1+=10;
+
+console.log(var1);
+
+
+
+
+
+
+
